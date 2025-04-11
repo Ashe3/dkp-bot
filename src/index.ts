04f140a71831405d1f9b4507d9027353e3ab310d
Anton Shevchenko <korolewsky.anton3@gmail.com>
2025-04-11 13:45:28 +0300

    added fake host for bot

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import http from "http";
 import dotenv from "dotenv";
 dotenv.config();
 
@@ -6,6 +7,13 @@ import { bot } from "./bot";
 bot.launch();
 
 console.log("🤖 is running");
+
+http
+  .createServer((req, res) => {
+    res.writeHead(200);
+    res.end("Bot is running");
+  })
+  .listen(Number(process.env.PORT || 3000), "0.0.0.0");
 
 process.once("SIGINT", () => bot.stop("SIGINT"));
 process.once("SIGTERM", () => bot.stop("SIGTERM"));
