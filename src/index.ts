6de58071e3732cabae7402d1b5f0c6f1109d356d
Anton Shevchenko <korolewsky.anton3@gmail.com>
2025-04-11 17:03:12 +0300

    use webhook bot

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,26 @@
 import http from "http";
 import dotenv from "dotenv";
+import express from "express";
+
 dotenv.config();
 
 import { bot } from "./bot";
+const app = express();
+app.use(bot.webhookCallback("/webhook"));
 
-bot.launch();
+bot.telegram
+  .setWebhook(`${process.env.WEBHOOK_URL}/webhook`)
+  .then(() => console.log("✅ Webhook set"))
+  .catch((err) => console.error("❌ Failed to set webhook", err));
 
-console.log("🤖 is running");
+app.get("/", (_, res) => {
+  res.send("Telegram bot (Webhook mode) is active");
+});
 
-http
-  .createServer((req, res) => {
-    res.writeHead(200);
-    res.end("Bot is running");
-  })
-  .listen(Number(process.env.PORT || 3000), "0.0.0.0");
+const PORT = Number(process.env.PORT) || 3000;
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`🚀 Server listening on port ${PORT}`);
+});
 
 process.once("SIGINT", () => bot.stop("SIGINT"));
 process.once("SIGTERM", () => bot.stop("SIGTERM"));
